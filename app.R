fde9b13d49f375a571d5a0da398c58bb52544df2
Joey Mays <joeymays94@gmail.com>
2022-06-18 16:21:10 -0400

    minor UI rearragement

--- app.R
+++ app.R
@@ -9,44 +9,44 @@ options(repos = BiocManager::repositories())
 
 source("getGeneMetadata.R")
 source("metadataprocessing.R")
+hg19.gene.lookup <- readRDS("hg19-gene-lookup.RDS")
 
 library(shiny)
 library(shinythemes)
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("darkly"),
-    
-HTML(r"(<p style="text-align:center;">Joey Mays - Updated 2022-02-07</p>)"),
                 
-    # Application title
-    titlePanel("Get Chromosomes", windowTitle = "GetChromosomes"),
-
-    mainPanel(
-        p("This tool takes a .csv file with a column of gene symbols and adds genomic metadata including chromsomes, cytobands, ensembl IDs, and locations for each gene."),
-        p("It will also attempt to correct gene symbol aliases to the accepted HGNC symbol."),
-        p("The query may take a (literal) minute or two to complete after clicking", strong("\"Run\".")),
-        p("Note: Currently supports only gene symbols as input (i.e. no ensembl IDs)."),
-        
-        hr(),
-        
-        fileInput(inputId = "inputCSV", label = "Upload File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-            
-        selectInput("columnNameInput", "Select Gene Name Column", choices = NULL),
-        
-        radioButtons("symbolClass", "Select Symbol Class", choices = c("Gene Symbol"),),
-        
-        
-        actionButton(inputId = "runTool", label = "Run: Get Metadata", icon("fas fa-running"), style="color: #fff; background-color: #00bc8c; border-color: #00a87d"),
-        
-        hr(),
-        
-        textOutput(outputId = "readyFlag"),
-        
-        
-        downloadButton(outputId = "outputCSV", label = "Download Metadata", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-        ),
-    
-    )
+                HTML(r"(<p style="text-align:center;">Joey Mays - Updated 2022-06-17</p>)"),
+                
+                # Application title
+                titlePanel("Get Chromosomes", windowTitle = "GetChromosomes"),
+                sidebarLayout(
+                    sidebarPanel(fileInput(inputId = "inputCSV", label = "Upload File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+                                 
+                                 selectInput("columnNameInput", "Select Gene Name Column", choices = NULL),
+                                 
+                                 radioButtons("symbolClass", "Select Symbol Class", choices = c("Gene Symbol"),),
+                                 
+                                 
+                                 actionButton(inputId = "runTool", label = "Run: Get Metadata", icon("fas fa-running"), style="color: #fff; background-color: #00bc8c; border-color: #00a87d"),
+                                 
+                                 hr(),
+                                 
+                                 textOutput(outputId = "readyFlag"),
+                                 
+                                 
+                                 downloadButton(outputId = "outputCSV", label = "Download Metadata", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                    ),
+                mainPanel(
+                    p("This tool takes a .csv file with a column of gene symbols and adds genomic metadata including chromsomes, cytobands, ensembl IDs, and locations for each gene."),
+                    p("It will also attempt to correct gene symbol aliases to the accepted HGNC symbol."),
+                    p("The query may take a (literal) minute or two to complete after clicking", strong("\"Run\".")),
+                    p("Note: Currently supports only gene symbols as input (i.e. no ensembl IDs)."),
+                    hr(),
+                ),
+)
+)
 
 # Define server logic
 server <- function(input, output) {
